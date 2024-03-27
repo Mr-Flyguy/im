72e3cb1a5543f564c288e6af1b1801203f818aaf
MEGAPOLIS <musaevmaksimm@gmail.com>
2024-03-27 17:29:42 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	task5.py

--- task1.py
+++ task1.py
@@ -7,9 +7,11 @@ for i in austronaut_time[1:]:  # Проходимя по таблице
     time = [num for num in temp[-2].split(':')]  # Получаем значения часов, минут и секунд в строковом виде
     hour = int(time[0])  # Переводим значения часов в целочисленный тип
     if hour + int(temp[-1]) >= 10:  # Формируем актуальное время
-        temp[-1] = f'{(int(temp[-1]) + int(time[0])) % 24}:{time[1]}:{time[2]}'
+        temp[-1] = ('0' if (temp_time:=(int(temp[-1]) + int(
+            time[0])) % 24) < 10 else '') + f'{temp_time%24}:{time[1]}:{time[2]}'
     else:
-        temp[-1] = f'{(int(temp[-1]) + int(time[0])) % 24}:{time[1]}:{time[2]}'
+        temp[-1] = ('0' if (temp_time :=(int(temp[-1]) + int(
+            time[0]))) % 24 < 10 else '') + f'{temp_time%24}:{time[1]}:{time[2]}'
     if temp[2] == '98-OYE':  # Ищем актуальное время для заданной каюты
         print(f'{temp[-1]} - действительное время для каюты: {temp[2]}')
     new_time.write(','.join(temp) + '\n')  # Записываем измененную строку в новую таблицу
